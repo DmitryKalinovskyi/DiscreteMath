c79b6b323f23dd2506b369eb23f8040a808ba624
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-04-17 15:35:05 +0300

    Added matrix relation example

---
+++ DiscreteMathApplications/Lab02 Matrix Relation/MatrixRelation/Services/IMatrixRelationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace MatrixRelation.Services
+{
+    public interface IMatrixRelationService<T>
+    {
+        public bool IsHaveRelation(T a, T b);
+        public bool[,] GetRelationMatrix(IList<T> a, IList<T> b);
+    }
+}
---
+++ DiscreteMathApplications/Lab02 Matrix Relation/MatrixRelation/Services/MatrixRelationService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixRelation.Services
+{
+    public class MatrixRelationService<T> : IMatrixRelationService<T>
+    {
+        private Func<T, T, bool>? _relationCondition;
+
+        public MatrixRelationService()
+        {
+            _relationCondition = null;
+        }
+
+        public MatrixRelationService(Func<T, T, bool> relationCondition)
+        {
+            _relationCondition = relationCondition;
+        }
+
+        public void SetRelationCondition(Func<T, T, bool> relationCondition)
+        {
+            _relationCondition = relationCondition;
+        }
+
+        public bool[,] GetRelationMatrix(IList<T> a, IList<T> b)
+        {
+            bool[,] result = new bool[a.Count(), b.Count()];
+
+            for (int i = 0; i < a.Count(); i++)
+            {
+                for (int j = 0; j < b.Count(); j++)
+                {
+                    result[i, j] = IsHaveRelation(a[i], b[j]);
+                }
+            }
+
+            return result;
+        }
+
+        public bool IsHaveRelation(T a, T b)
+        {
+            // by default all elements don't have relation
+            return _relationCondition?.Invoke(a, b) ?? false;
+        }
+    }
+}
